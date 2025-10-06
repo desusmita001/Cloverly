5d59888d3b65acb755923cd5991659a7d60bdeea
Susmita De <desusmita001@gmail.com>
2025-10-06 14:29:24 -0500

    Initial Commit

---
+++ Cloverly/Cloverly/CloverlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloverlyApp.swift
+//  Cloverly
+//
+//  Created by Susmita De on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct CloverlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
